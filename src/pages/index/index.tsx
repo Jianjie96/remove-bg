6ada5c3af87cb4751bc07e998517254840c4ab5c
Jianjie96 <awesometangji@gmail.com>
2026-01-14 16:28:11 +0800

    feat: 获取图片

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { View, Text } from '@tarojs/components'
+import { View, Text, Button, Image } from '@tarojs/components'
 import Taro, { useLoad } from '@tarojs/taro'
 import { RemoveBgClient, type AccountInfo } from '@/api';
 import './index.scss'
@@ -8,6 +8,8 @@ const RemoveBgClientInstance = new RemoveBgClient();
 
 export default function Index () {
   const [accoutInfo, setAccoutInfo] = useState<Partial<AccountInfo['data']['attributes']>>({});
+  const [localImgList, setLocalImgList] = useState<Taro.chooseImage.ImageFile[]>([]);
+
   useLoad(async () => {
     const res = await RemoveBgClientInstance.getAccount();
     setAccoutInfo(res.data.attributes)
@@ -20,9 +22,19 @@ export default function Index () {
     return count
   }
 
+  function handleUpload() {
+    Taro.chooseImage({
+      success(res) {
+        setLocalImgList(res.tempFiles)
+      }
+    })
+  }
+
   return (
     <View className='index'>
       <Text>当前积分剩余{ calcCount() }</Text>
+      <Button onClick={handleUpload}>选择图片</Button>
+      { localImgList.map(img => <Image src={img.path} key={img.path}></Image>) }
     </View>
   )
 }
