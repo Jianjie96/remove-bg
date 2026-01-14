3d8444233e6905cf41c410184440cd15a097a212
Jianjie96 <awesometangji@gmail.com>
2026-01-14 16:17:08 +0800

    feat: 增加积分计数

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,18 +1,28 @@
+import { useState } from 'react';
 import { View, Text } from '@tarojs/components'
 import Taro, { useLoad } from '@tarojs/taro'
-import { RemoveBgClient } from '@/api';
+import { RemoveBgClient, type AccountInfo } from '@/api';
 import './index.scss'
 
 const RemoveBgClientInstance = new RemoveBgClient();
 
 export default function Index () {
+  const [accoutInfo, setAccoutInfo] = useState<Partial<AccountInfo['data']['attributes']>>({});
   useLoad(async () => {
     const res = await RemoveBgClientInstance.getAccount();
+    setAccoutInfo(res.data.attributes)
   })
+
+  function calcCount() {
+    let count = 0;
+
+    count = (accoutInfo.api?.free_calls || 0) + (accoutInfo.credits?.total || 0)
+    return count
+  }
 
   return (
     <View className='index'>
-      <Text>甜甜，嘻嘻😁</Text>
+      <Text>当前积分剩余{ calcCount() }</Text>
     </View>
   )
 }
